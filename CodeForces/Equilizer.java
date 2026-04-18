b383fb1e31a18cdca420969f22677b91c1fb4319
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-19 04:45:06 +0530

    adding some few linkedlist code files

--- CodeForces/Equilizer.java
+++ CodeForces/Equilizer.java
@@ -1,25 +1,25 @@
+import java.util.*;
+
 public class Equilizer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt(); // number of test cases
+        int t = sc.nextInt();
 
         while (t-- > 0) {
             int n = sc.nextInt();
             int k = sc.nextInt();
 
-            int[] array = new int[n];
             int sum = 0;
 
             for (int i = 0; i < n; i++) {
-                array[i] = sc.nextInt();
-                sum += array[i];
+                sum += sc.nextInt();
             }
 
             if (sum % 2 == 1) {
                 System.out.println("YES");
             } 
-            else if (n % 2 == 1 && k % 2 == 1) {
+            else if ((n * k - 1) % 2 == 1) {
                 System.out.println("YES");
             } 
             else {
