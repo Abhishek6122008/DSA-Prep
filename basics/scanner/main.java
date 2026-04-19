55d9e94bfd258cea757914dbb8890155f82777a1
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-20 04:40:03 +0530

    Java basics

---
+++ basics/scanner/main.java
@@ -0,0 +1,17 @@
+package basics.scanner;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //nextInt()
+        //nextFloat()
+        System.out.println(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+
+        
+    }
+}
