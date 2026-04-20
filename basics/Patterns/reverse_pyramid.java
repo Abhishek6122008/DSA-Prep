a02f85a342c57eab44802702b9ac8a50cf40eee5
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-21 03:24:52 +0530

    Java basics

--- basics/Patterns/reverse_pyramid.java
+++ basics/Patterns/reverse_pyramid.java
@@ -3,7 +3,7 @@ package basics.Patterns;
 public class reverse_pyramid {
     public static void main(String args[]){
         int n = 4;
-        for(int i =1;i<=n;i++){
+        for(int i =n;i>=1;i--){
             for(int j = 1;j<=i;j++){
                 System.out.print("*");
             }System.out.println();
