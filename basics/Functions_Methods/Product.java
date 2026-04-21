a7993435db993e75ee86198d9911ec55c73d921c
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-22 02:35:21 +0530

    Java basics

---
+++ basics/Functions_Methods/Product.java
@@ -0,0 +1,14 @@
+package basics.Functions_Methods;
+import java.util.*;
+public class Product {
+    public static void productOfTwo(int a, int b){
+        System.out.println(a*b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        productOfTwo(a,b);
+
+    }
+}
