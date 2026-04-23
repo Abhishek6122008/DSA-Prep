68a2ffed5a4477bb128e68aa9f6b9fb8ce02c001
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-23 22:28:15 +0530

    Java basics

---
+++ basics/Arrays.java
@@ -0,0 +1,17 @@
+package basics;
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 83;
+        marks[1] = 73;
+        marks[2] = 23;
+////        System.out.println(marks[0]);
+////        System.out.println(marks[1]);
+////        System.out.println(marks[2]);
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
