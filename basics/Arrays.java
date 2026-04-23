e36da394545b0dabb0729271d83e4b487bb4d1bb
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-04-23 22:39:56 +0530

    Java basics

--- basics/Arrays.java
+++ basics/Arrays.java
@@ -1,17 +1,31 @@
 package basics;
 import java.util.*;
+
 public class Arrays {
     public static void main(String[] args) {
-        int[] marks = new int[3];
-        marks[0] = 83;
-        marks[1] = 73;
-        marks[2] = 23;
-////        System.out.println(marks[0]);
-////        System.out.println(marks[1]);
-////        System.out.println(marks[2]);
-        for(int i=0;i<3;i++){
-            System.out.println(marks[i]);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of items you want in array: ");
+        int size = sc.nextInt();
+
+        int[] numbers = new int[size];
+
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter element " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
         }
 
+        System.out.print("Enter number to search: ");
+        int same = sc.nextInt();
+
+        System.out.println("Index positions where the number is found:");
+
+        for(int i = 0; i < size; i++){
+            if(numbers[i] == same){
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
     }
 }
