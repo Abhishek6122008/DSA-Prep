b6db514cab22e809921d6e82a932888c7fee5e14
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-05-11 13:57:42 +0530

    cf questions

---
+++ CodeForces/firstUniqueChar.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class firstUniqueChar {
+    public static void main(String[] args) {
+        String s = "codingminutes";
+        HashMap<Character,Integer> hm= new HashMap<>();
+        for (int i = 0; i <s.length() ; i++) {
+            char ch = s.charAt(i);
+            if(hm.containsKey(ch)){
+                hm.put(ch,hm.get(ch)+1);
+            }else{
+                hm.put(ch,1);
+            }
+        }
+        int unique = -1;
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(hm.get(ch) == 1) {
+                unique = hm.get(ch)-1;
+            }
+        }
+
+        System.out.println(unique);
+    }
+}
