5f1627ca5a17d49890b9285aae06196b7359e5d9
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-05-20 10:11:40 +0530

    String  questions

--- Leetcode/longestCommonPrefix.java
+++ Leetcode/longestCommonPrefix.java
@@ -2,6 +2,17 @@ package Leetcode;
 
 public class longestCommonPrefix {
     public static void main(String[] args) {
-        
+        String strs[] = {"flower","flow","flight"};
+        String ans = "";
+        for (int i = 0; i <strs[0].length() ; i++) {
+            char ch = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if(i>=strs[j].length()||strs[j].charAt(i)!=ch){
+                    System.out.println(ans);
+                    return;
+                }
+            }
+            ans+=ch;
+        }
     }
 }
