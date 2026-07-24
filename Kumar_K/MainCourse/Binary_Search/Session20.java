1006787c503eaf8d747416ea10427cf6780c918e
Abhishek Chaudhary <abhishek.6122008@gmail.com>
2026-07-24 21:23:10 +0530

    simple easy questions

---
+++ Kumar_K/MainCourse/Binary_Search/Session20.java
@@ -0,0 +1,7 @@
+package Kumar_K.MainCourse.Binary_Search;
+
+public class Session20 {
+    static void main(String[] args) {
+        
+    }
+}
---
+++ Leetcode/printAllSubsequences.java
@@ -0,0 +1,12 @@
+package Leetcode;
+
+public class printAllSubsequences {
+    static void main(String[] args) {
+        message(1);
+    }
+    static void message(int n){
+        if(n>5) return;
+        System.out.println(n);
+        message(n+1);
+    }
+}
